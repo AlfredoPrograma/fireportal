9ee0aed18f6e0e381bc3a35dc6f3f9c86204ee24
Alfredo Arvelaez <alfredoprograma.dev@gmail.com>
2025-09-09 14:42:33 -0400

    fix(backend): rewrite migration file to use SQLITE utilities

--- backend/.migrations/0001_create_incidents_table.sql
+++ backend/.migrations/0001_create_incidents_table.sql
@@ -1,8 +1,8 @@
 CREATE TABLE incidents (
-    id SERIAL PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    incident_type VARCHAR(100) NOT NULL,
-    location VARCHAR(255),
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    incident_type TEXT NOT NULL,
+    location TEXT,
     description TEXT,
     image TEXT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
