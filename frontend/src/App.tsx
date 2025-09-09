d8896c050d5c842ca7f32790b4eefd5de9a44961
Alfredo Arvelaez <alfredoprograma.dev@gmail.com>
2025-09-08 23:42:53 -0400

    chores(frontend): install and set up http layer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,11 @@
-import { Button } from './components/ui/button'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 export function App() {
-  return <Button>Hello world</Button>
+  return (
+    <QueryClientProvider client={queryClient}>
+      <h1>Literally all the application</h1>
+    </QueryClientProvider>
+  )
 }
---
+++ frontend/src/lib/api.ts
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+/**
+ * Axios instance pre-configured with the API base URL.
+ *
+ * The base URL is determined by the `VITE_API_URL` environment variable.
+ * If the environment variable is not set, it defaults to `'http://localhost:8080/api/v1'`.
+ *
+ */
+export const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080/api/v1',
+})
